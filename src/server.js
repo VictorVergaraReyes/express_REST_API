3838b5ff1ddde8fab95bcc1e31a4b14f34a63dca
vicvergara <vvergara@ahorra.io>
2023-07-18 16:34:55 -0600

    variables de entorno

--- src/server.js
+++ src/server.js
@@ -1,18 +1,6 @@
-import express from 'express';
-import cartasRoutes from './routes/cartas.routes.js'
-import indexRoutes from './routes/index.routes.js'
-const app=express()
+import app from "./app"
+import { PORT } from "./config"
 
-app.use(express.json())
-app.use(indexRoutes)
-app.use('/api',cartasRoutes)
-
-app.use((req,res,next) => {
-    res.status(404).json({
-        message:'endpoint not found'
-    })
-})
-
-app.listen(3000)
-console.log("server on port 3000")
+app.listen(PORT)
+console.log("server on port ".PORT)
  
