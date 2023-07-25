e9494b6ee05fb534cc018774589889705b156fd4
vicvergara <vvergara@ahorra.io>
2023-07-25 16:58:27 -0600

    conexion db remoto

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 import app from "./app.js"
-import { PORT } from "./config.js"
+import { PORT, DB_USER, DB_HOST, DB_DATABASE} from "./config.js"
 
 app.listen(PORT)
 console.log("server on port ",PORT)
- 
+console.log("server on DB_USER ",DB_USER)
+console.log("server on DB_HOST ",DB_HOST)
+console.log("server on DB_DATABASE ",DB_DATABASE)
