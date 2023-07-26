4d5b09e1fd4e812a308b2634ca334fd5715faf8a
vicvergara <vvergara@ahorra.io>
2023-07-26 16:49:33 -0600

    añadiendo cors para todas las url

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import express from 'express';
+import cors from 'cors'
 import cartasRoutes from './routes/cartas.routes.js'
 import indexRoutes from './routes/index.routes.js'
 import './config.js'
 import { PORT } from './config.js';
 const app=express()
+app.use(cors());
 
 app.use(express.json())
 app.use(indexRoutes)
